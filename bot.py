7878bd1d3b66cdeb1a87812d5b77a36b11381f63
TheEmporor <35822194+TheEmporor@users.noreply.github.com>
2018-01-25 19:43:22 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+imports
+import discord
+import sys
+import os 
+import io
+from discord.ext import commands
+bot = commands.Bot(command_prefix=('[THE KING]'),description="[your dream bot] \n\nHelp Commands",owner_id=[250674147980607488])
