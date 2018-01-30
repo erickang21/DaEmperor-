569a29847ea6fb8099524b05e938e6f0cedaf66e
TheEmporor <35822194+TheEmporor@users.noreply.github.com>
2018-01-29 19:22:54 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,14 @@ async def commandname(ctx):
     print("no token found REEEE!")
 bot.run(os.environ.get('TOKEN').strip('"'))
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('!!'),description="yeeee\n\nHelp Commands",owner_id=250674147980607488)
+@bot.event
+async def on_ready():
+    print('Bot is online, and ready to ROLL!')
+    await bot.change_presence(game=discord.Game(name=f"with {len(bot.guilds)} servers!")) 
+    @bot.command
+async def ping(ctx):
+    await ctx.send("Ello peeps! I'm online
+                   if not os.environ.get('TOKEN'):
+    print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
     
