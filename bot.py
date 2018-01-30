acf6ac62dd1b15a57a391b8ccb5a77cd74d39e5c
TheEmporor <35822194+TheEmporor@users.noreply.github.com>
2018-01-29 18:10:41 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,3 +5,11 @@ import os
 import io
 from discord.ext import commands
 bot = commands.Bot(command_prefix=('[!!]'),description="[your dream bot] \n\nHelp Commands",owner_id=[250674147980607488])
+@bot.command()
+async def commandname(ctx):
+    code goes here
+    await ctx.send("msg does here")
+    if not os.environ.get('TOKEN'):
+    print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
+    
